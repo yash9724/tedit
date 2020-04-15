f5ef76ff6a728a5d005666025acdd4056a68dc53
Yash <yashgupta9724@gmail.com>
2020-04-15 19:31:10 +0530

    Feature to process Home, End, Page Up, Page Down keys added.

--- tedit.c
+++ tedit.c
@@ -7,16 +7,34 @@
 #include<sys/ioctl.h>
 #include<string.h>
 
+#define TEDIT_VERSION "0.0.1"
+#define CTRL_KEY(k) ((k) & 0x1f)           // turns off bit 7, 6 and 5 of the char
 
-#define CTRL_KEY(k) ((k) & 0x1f)      // turns off bit 7, 6 and 5 of the char
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
 
+
+// Keeps track of global editor state
 struct editorConfig{
+    // cx --> horizontal coordinate of cursor(columns) 
+    // cy --> vertical coordinate of cursor(rows)
+    int cx, cy;      
     int screenrows;
     int screencols;
     struct termios orig_termois;
 };
 
-struct editorConfig E;
+
+struct editorConfig E;   // global editor state object
 
 /*** append buffer **/
 struct abuf{
@@ -41,22 +59,66 @@ void abFree(struct abuf *ab){
 }
 
 void die(char *s){
-    write(STDOUT_FILENO, "\x1b[2J",4);  
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    // write(STDOUT_FILENO, "\x1b[2J",4);  
+    // write(STDOUT_FILENO, "\x1b[H", 3);
     
     perror(s);
     exit(1);
 }
 
 
-char editorReadKey(){
+int editorReadKey(){
     int nread;
     char c;
     while((nread = read(STDIN_FILENO, &c, 1)) != 1){
         if(nread == -1 && errno != EAGAIN)
             die("read");
     }
-    return c;
+
+    if(c == '\x1b'){
+        char seq[3];
+        if(read(STDIN_FILENO, &seq[0], 1) != 1) 
+            return '\x1b';
+        if(read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+
+        if(seq[0] == '['){
+            if(seq[1] >= '0' && seq[1] <= '9'){
+                if(read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+                if(seq[2] == '~'){
+                    switch(seq[1]){
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            } else {
+                switch(seq[1]){
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        } else if(seq[0] == 'O') {
+            switch(seq[1]){
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+
+        }
+
+        return '\x1b'; 
+    } else {
+        return c;
+    }
 }
 
 
@@ -100,12 +162,10 @@ int getWindowSize(int *rows, int *cols){
     }
 }
 
-
 void disableRawMode(){
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termois) == -1)
         die("tcsetattr");
 }
-
 
 void enableRawMode(){
     if(tcgetattr(STDIN_FILENO, &E.orig_termois) == -1)
@@ -124,14 +184,62 @@ void enableRawMode(){
 
 }
 
+void editorMoveCursor(int key){
+    switch(key){
+        case ARROW_LEFT:
+            if(E.cx != 0){
+                E.cx--;
+            }    
+            break;
+        case ARROW_RIGHT:
+            if(E.cx != E.screencols - 1){
+                E.cx++;
+            }
+            break;
+        case ARROW_UP:
+            if(E.cy != 0){
+                E.cy--;
+            }
+            break;
+        case ARROW_DOWN:
+            if(E.cy != E.screenrows - 1){
+                E.cy++;
+            }
+            break;
+    }
+}
 
 void editorProcessKeypress(){
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch(c){
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J",4);  
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;    
+        
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while(times--)
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+
+        case ARROW_UP:
+        case ARROW_LEFT:
+        case ARROW_DOWN:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
             break;
     }
 }
@@ -140,7 +248,23 @@ void editorProcessKeypress(){
 void editorDrawRows(struct abuf *ab){
     int y;
     for(y = 0; y < E.screenrows ; y++){
-        abAppend(ab, "~", 1);
+        if(y == E.screenrows/3){
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Tedit Editor -- version %s", TEDIT_VERSION);
+            if(welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if(padding){
+                abAppend(ab,"~",1);
+                padding--;
+            }
+            while(padding--)
+                abAppend(ab," ",1);
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
+        abAppend(ab,"\x1b[K",3);
 
         if(y < E.screenrows - 1){
             abAppend(ab, "\r\n", 2);
@@ -154,12 +278,14 @@ void editorRefreshScreen(){
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH",E.cy+1,E.cx+1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -168,6 +294,8 @@ void editorRefreshScreen(){
 
 
 void initEditor(){
+    E.cx = 0;
+    E.cy = 0;
     if(getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
