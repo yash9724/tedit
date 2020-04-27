1a1684e964c813836172e9ea61f8e837a2f0ddd3
Yash <yashgupta9724@gmail.com>
2020-04-27 09:05:31 +0530

    Enter and Backspace mapped.

--- tedit.c
+++ tedit.c
@@ -19,6 +19,7 @@
 
 #define TEDIT_VERSION "0.0.1"
 #define TAB_STOP 8
+#define QUIT_TIMES 2
 #define CTRL_KEY(k) ((k) & 0x1f)           // turns off bit 7, 6 and 5 of the char
 
 enum editorKey {
@@ -268,9 +269,13 @@ void editorUpdateRow(erow *row){
     row->rsize = index;
 }
 
-void editorAppendRow(char *s, size_t len){
+void editorInsertRow(int at, char *s, size_t len) {
+    if(at < 0 || at > E.numrows)
+        return;
+    
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
-    int at = E.numrows;
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
     E.row[at].size = len;
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
@@ -280,6 +285,20 @@ void editorAppendRow(char *s, size_t len){
     E.row[at].render = NULL;
     editorUpdateRow(&E.row[at]);
     E.numrows++;
+    E.dirty++;
+}
+
+void editorFreeRow(erow *row) {
+    free(row->render);
+    free(row->chars);
+}
+
+void editorDelRow(int at){
+    if(at < 0 || at >= E.numrows)
+        return;
+    editorFreeRow(&E.row[at]);
+    memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+    E.numrows--;
     E.dirty++;
 }
 
@@ -294,17 +313,65 @@ void editorRowInsertChar(erow *row, int at, int c)  {
     E.dirty++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len) {
+    row->chars = realloc(row->chars, row->size + len + 1);
+    memcpy(&row->chars[row->size], s, len);
+    row->size += len;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+    E.dirty++;
+}
+
+void editorRowDelChar(erow *row, int at) {
+  if (at < 0 || at >= row->size) return;
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 /*** editor operations ***/
 
 void editorInsertChar(int c){
     if(E.cy == E.numrows) {
-        editorAppendRow("",0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
 }
 
+void editorInsertNewline(){
+    if(E.cx == 0){
+        editorInsertRow(E.cy, "", 0);
+    } else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
+}
 
+void editorDelChar() {
+  if (E.cy == E.numrows) 
+    return;
+  if (E.cx == 0 && E.cy == 0)
+    return;
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
+  }
+}
 
 /*** file i/o ***/
 
@@ -368,7 +435,7 @@ void editorOpen(char *filename) {
                               line[linelen - 1] == '\r'))
             linelen--;
 
-        editorAppendRow(line, linelen);
+        editorInsertRow(E.numrows, line, linelen);
     }
 
     free(line);
@@ -418,13 +485,20 @@ void editorMoveCursor(int key){
 }
 
 void editorProcessKeypress(){
+    static int quit_times = QUIT_TIMES;
     int c = editorReadKey();
     switch(c){
         case '\r':
-            /* TODO */
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
+            if(E.dirty && quit_times > 0){
+                editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                "Press Ctrl-Q %d more times to quit.", quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J",4);  
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
@@ -438,11 +512,12 @@ void editorProcessKeypress(){
             E.cx = 0;
             break;
 
-        
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
-            /* TODO */
+            if (c == DEL_KEY)
+                editorMoveCursor(ARROW_RIGHT);
+            editorDelChar();
             break;
 
         case END_KEY:
@@ -481,6 +556,8 @@ void editorProcessKeypress(){
             editorInsertChar(c);
             break;
     }
+
+    quit_times = QUIT_TIMES;
 }
 
 /*** output ***/
